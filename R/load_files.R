dee86190666fc96a27826fd305110e128b233cdb
Emil Bjerglund Pedersen <emilbp@gmail.com>
2017-03-09 16:23:44 +0100

    First working example of gRdata_read()

--- R/load_files.R
+++ R/load_files.R
@@ -1,6 +1,8 @@
 #' Load results of graphene curve fit
 #'
 #' Attempts to load all curve fits in txt files from the specified directory
+#'
+#' @importFrom magrittr %>%
 #' @param path Directory containing the data-files
 #' @param ext Extension of data-files (default is 'txt')
 #' @param peaks Regex string detailing the peaks to look for
@@ -12,17 +14,16 @@
 
 
 gRdata_read <- function(path, ext = "txt", peaks = "^((D\\s)|(G\\s)|(2D\\s)|(aC\\s)|(DDp\\s)|(2Dp\\s)|(2Dpp\\s)|(SiO2\\s))", measure = "FWHM|pos|int") {
-  filenames <- list.files(pattern = str_c('*.',ext), path = path)
-  data <- tibble(filename = filenames) %>%
-    mutate(path = str_c(path, "/", filename),
-      data = map(path, read_tsv, col_names = c("x", "y", "value"), skip = 1),
-      measure = str_c(str_extract(filename, peaks), str_extract(filename, measure))) %>%
-    unnest() %>%
-    mutate(xy = str_c(x, ", ", y)) %>%
-    #       id = unique(xy) %>% length() %>% seq(1, ., 1) %>% rep(15)) %>%
-    select(xy, x, y, measure, value) %>%
-    spread(key = measure, value = value) %>%
-    mutate(`D/G-ratio` = `D int` / `G int`,
+  filenames <- list.files(pattern = stringr::str_c('*.',ext), path = path)
+  data <- tibble::tibble(filename = filenames) %>%
+    dplyr::mutate(path = stringr::str_c(path, "/", filename),
+      data = purrr::map(path, readr::read_tsv, col_names = c("x", "y", "value"), skip = 1),
+      measure = stringr::str_c(stringr::str_extract(filename, peaks), stringr::str_extract(filename, measure))) %>%
+    tidyr::unnest() %>%
+    dplyr::mutate(xy = stringr::str_c(x, ", ", y)) %>%
+    dplyr::select(xy, x, y, measure, value) %>%
+    tidyr::spread(key = measure, value = value) %>%
+    dplyr::mutate(`D/G-ratio` = `D int` / `G int`,
       `2D/G-ratio` = `2D int` / `G int`)
   data
 }
