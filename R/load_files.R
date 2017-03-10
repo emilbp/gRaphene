223593ff7a482de5e38318e54875bb1f32873d35
Emil Bjerglund Pedersen <emilbp@gmail.com>
2017-03-10 15:09:49 +0100

    Renamed functions. gr_map_ works with spectrum maps, and gr_cf_ works with folders containing curve fit results.

--- R/load_files.R
+++ R/load_files.R
@@ -2,6 +2,9 @@
 #'
 #' Attempts to load all curve fits in txt files from the specified directory
 #'
+#' All files should be named as "<peak> <feature>.txt", e.g. "2D FWHM.txt" and be put in the folder specified under \code{path}
+#' The script automatically looks for the following peaks (unless other parameters are stated under \code{peaks}): D, G, 2D, aC (amorphous carbon), DDp (D+D'), 2Dp (2D'), 2Dpp (2D''), SiO2. It automatically tries to register the features: FWHM, pos and int.
+#'
 #' @importFrom magrittr %>%
 #' @param path Directory containing the data-files
 #' @param ext Extension of data-files (default is 'txt')
@@ -10,10 +13,11 @@
 #' @keywords raman, curve fit
 #' @export
 #' @examples
-#' gRdata_read()
+#' gr_cf_read("data")
+#' gr_cf_read(system.file('extdata/graphene_curve_fit_export', package = 'gRaphene'))
 
 
-gRdata_read <- function(path, ext = "txt", peaks = "^((D\\s)|(G\\s)|(2D\\s)|(aC\\s)|(DDp\\s)|(2Dp\\s)|(2Dpp\\s)|(SiO2\\s))", measure = "FWHM|pos|int") {
+gr_cf_read <- function(path, ext = "txt", peaks = "^((D\\s)|(G\\s)|(2D\\s)|(aC\\s)|(DDp\\s)|(2Dp\\s)|(2Dpp\\s)|(SiO2\\s))", measure = "FWHM|pos|int") {
   filenames <- list.files(pattern = stringr::str_c('*.',ext), path = path)
   data <- tibble::tibble(filename = filenames) %>%
     dplyr::mutate(path = stringr::str_c(path, "/", filename),
@@ -31,14 +35,17 @@ gRdata_read <- function(path, ext = "txt", peaks = "^((D\\s)|(G\\s)|(2D\\s)|(aC\
 #' Load complete spectrum map from exported txt file
 #'
 #' Load all spectra and corresponding (x,y) coordinates and wrangles into a tidy tibble
+#' Expects a tab-separated file with 4 columns (5 if z-value is present - not implemented yet!)
+#'
 #' @importFrom magrittr %>%
 #' @param path Path to txt file containing the spectrum map
 #' @keywords raman, spectrum map
 #' @export
 #' @examples
-#' gRdata_map_read()
+#' gr_map_read("graphene_map.txt")
+#' gr_map_read(system.file('extdata', 'graphene_spectrum_map_export.txt', package = 'gRaphene'))
 
-gRdata_map_read <- function(path) {
+gr_map_read <- function(path) {
   data <- readr::read_tsv(path, col_names = c('x', 'y', 'wavenumber', 'intensity'), skip = 1) %>%
     dplyr::mutate(id = stringr::str_c(x, ", ", y)) %>%
     tidyr::spread(key = wavenumber, value = intensity) %>%
