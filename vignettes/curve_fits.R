b2af3f1162b221b34813f0258275616ac28b3fa4
Emil Bjerglund Pedersen <emilbp@gmail.com>
2017-03-30 15:32:28 +0200

    New Build

--- vignettes/curve_fits.R
+++ vignettes/curve_fits.R
@@ -28,3 +28,30 @@ data_cf %>%
   mutate(`2D/G-ratio` = ifelse(condition, `2D/G-ratio`, NA)) %>% 
   gr_cf_plot_all()
 
+## ---- fig.width = 7, fig.height=3, message=FALSE-------------------------
+curvefit <- system.file("extdata/curvefit_data.txt", package = "gRaphene")
+spectrum <- system.file("extdata/spectrum_example.txt", package = "gRaphene")
+
+gr_cf_fit_plot(spectrum, curvefit)
+
+## ---- fig.width = 7, fig.height=3, message=FALSE-------------------------
+gr_cf_ann <- gr_cf_fit_add(spectrum, curvefit) %>% 
+  select(-intensity, -envelope) %>% 
+  gather(key = "peak", value = "intensity", -wavenumber) %>% 
+  group_by(peak) %>% 
+  filter(intensity == max(intensity))
+
+gr_cf_fit_plot(spectrum, curvefit) +
+  ggthemes::theme_few() +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "none") +
+  scale_fill_brewer(palette = "Set3", guide = guide_legend(nrow = 1)) +
+  geom_text(data = gr_cf_ann, aes(x = wavenumber, y = intensity, label = peak), nudge_y = 40, nudge_x = 10)
+
+## ---- message=FALSE------------------------------------------------------
+fit_data <- gr_cf_fit_add(spectrum, curvefit)
+
+knitr::kable(head(fit_data, 5), digits = 3)
+
